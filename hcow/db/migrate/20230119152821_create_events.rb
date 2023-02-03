8eab87d9536f1bf6c64542482a22c275805c8515
Louis Houle <la.houle@baypath.net>
2023-02-03 17:48:40 +0000

    fixed up

---
+++ hcow/db/migrate/20230119152821_create_events.rb
@@ -0,0 +1,15 @@
+class CreateEvents < ActiveRecord::Migration[7.0]
+  def change
+    create_table :events do |t|
+      t.string :title
+      t.boolean :allday
+      t.datetime :eventstart
+      t.datetime :eventend
+      t.integer :occurence
+      t.text :details
+      t.boolean :featured
+
+      t.timestamps
+    end
+  end
+end
